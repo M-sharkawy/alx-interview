d9a8a3057c852273f35364b3572a933ad332b1c5
M-sharkawy <mostafasharks021@gmail.com>
2025-06-21 01:41:18 +0300

    island perimeter task

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""island perimeter module.
+"""
+
+def island_perimeter(grid):
+    """
+    returns the perimeter of the island described in grid
+    """
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0]) if rows > 0 else 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                # Check upper neighbor
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+
+                # Check left neighbor
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
